c4e5785f591b86e2bd1e9f6751e333287279e2cc
Kenneth Mensah <kenneth.mensah62@gmail.com>
2021-03-01 15:32:34 +0000

    Added functional interface

---
+++ src/com/java8/functionalinterface/MyFunctionalInterface.java
@@ -0,0 +1,26 @@
+package com.java8.functionalinterface;
+
+/**
+ * Functional interfaces have only one abstract method header
+ * it can have:
+ *      multiple default methods
+ *      multiple static methods
+ */
+
+@FunctionalInterface
+public interface MyFunctionalInterface {
+
+    void run();
+
+    default void firstDefault(){
+        System.out.println("First Default method!");
+    }
+
+    default void secondDefault(){
+        System.out.println("Second Default method!");
+    }
+
+    static void firstStaticMethod(){
+        System.out.println("First static method!");
+    }
+}
