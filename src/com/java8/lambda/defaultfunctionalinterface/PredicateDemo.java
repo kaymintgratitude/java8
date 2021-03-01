b36beed883cb7139fa7953a0a23f44922cb43f22
Kenneth Mensah <kenneth.mensah62@gmail.com>
2021-03-01 17:13:26 +0000

    Added predicate functional interface

--- src/com/java8/lambda/defaultfunctionalinterface/PredicateDemo.java
+++ src/com/java8/lambda/defaultfunctionalinterface/PredicateDemo.java
@@ -1,4 +1,16 @@
 package com.java8.lambda.defaultfunctionalinterface;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class PredicateDemo {
+
+    public static void main(String[] args){
+        Predicate<Integer> predicate = (integer) -> integer % 2 == 0;
+        System.out.println(predicate.test(10));
+
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        integerList.stream().filter(predicate).forEach(t-> System.out.println(String.format("Result: %b", t)));
+    }
 }
